174390a68436874482dd77b69b0368649b5a4fcb
Bruno Emanuel <brunoemanuel779@gmail.com>
2019-08-09 17:45:07 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+$('a[href^="#"]').click(function () {
+$('html, body').animate({
+scrollTop: $('[id="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+}, 1500);
+return false;
+});
+});
